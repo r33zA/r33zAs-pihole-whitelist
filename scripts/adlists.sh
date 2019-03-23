2fa466da61ffaf57b50b9b9c0989486937e12cfc
r33zA <47938622+r33zA@users.noreply.github.com>
2019-03-23 12:24:51 +1100

    Update adlists.sh

--- scripts/adlists.sh
+++ scripts/adlists.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script will download and add domains from the rep and replace installed adlists with my adlists.
-
 
 rm /etc/pihole/adlists.list
 curl -sS https://raw.githubusercontent.com/r33zA/r33zAs-pihole-whitelist/master/domains/adlists.list | sudo tee -a /etc/pihole/adlists.list >/dev/null
+curl -sSl https://raw.githubusercontent.com/mmotti/pihole-regex/master/install.sh | bash 
