b18956e5b5fe13b355fc9cbd8747df882f6d278f
r33zA <47938622+r33zA@users.noreply.github.com>
2019-03-26 16:10:40 +1100

    Update adlists.sh

--- scripts/adlists.sh
+++ scripts/adlists.sh
@@ -2,4 +2,4 @@
 
 rm /etc/pihole/adlists.list
 curl -sS https://raw.githubusercontent.com/r33zA/r33zAs-pihole-whitelist/master/domains/adlists.list | sudo tee -a /etc/pihole/adlists.list >/dev/null
-# curl -sSl https://raw.githubusercontent.com/mmotti/pihole-regex/master/install.sh | bash 
+curl -sSl https://raw.githubusercontent.com/mmotti/pihole-regex/master/install.sh | bash 
